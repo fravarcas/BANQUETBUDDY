0d29c83388d621667a39fba7d85b1c6a34a17ae1
jaicabher1 <jaicabher1@alum.us.es>
2024-02-26 11:50:22 +0100

    feat: Faq page completed

--- banquetBuddy/core/urls.py
+++ banquetBuddy/core/urls.py
@@ -6,6 +6,6 @@ from core.views import *
 urlpatterns = [
     path('', home, name='home'),
     path('about-us', about_us, name='about_us'),
-    path('preguntas-rapidas', faq, name='faq')
+    path('faq', faq, name='faq')
 
 ]
