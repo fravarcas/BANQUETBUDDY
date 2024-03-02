00c7df294b70ab144ac39988c12ad28ee746459a
edubusluc <edubusluc@alum.us.es>
2024-03-02 12:55:06 +0100

    feat: Add CSS login

--- banquetBuddy/core/forms.py
+++ banquetBuddy/core/forms.py
@@ -2,4 +2,9 @@ from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 
 class EmailAuthenticationForm(AuthenticationForm):
-    username = forms.EmailField(widget=forms.TextInput(attrs={'autofocus': True}))
+    username = forms.EmailField(
+        widget=forms.TextInput(attrs={'autofocus': True, 'placeholder': 'Email', 'class': 'rounded-input'})
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': 'Contraseña', 'class': 'rounded-input'})
+    )
